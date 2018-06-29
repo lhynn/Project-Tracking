5d8612f1c4bb66763a8aa73274ef5851a722ed56
lhynn <1872092733@163.com>
2018-06-29 14:54:15 +0800

    第二版终端见面设计OK

---
+++ 二版本4轮软件/Sec_Terminal_pth5_升级/sec_terminal.h
@@ -0,0 +1,177 @@
+#ifndef SEC_TERMINAL_H
+#define SEC_TERMINAL_H
+
+#include <QWidget>
+#include <QLabel>
+#include <QMatrix>
+#include <QDebug>
+#include <QString>
+#include <QVBoxLayout>
+#include <QListWidget>
+#include <QFileInfoList>
+#include <QMatrix>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QDialog>
+#include <QDir>
+#include <QSlider>
+#include <unistd.h>
+#include <QPainter>
+#include <QFrame>
+#include <QTimer>
+#include <QFileDialog>
+
+//#include "keydel.h"
+//#include "searchdir.h"
+#include <QByteArray>
+extern "C" {
+    #include <sys/vfs.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <dirent.h>
+    #include <string.h>
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <fcntl.h>
+    #include <unistd.h>
+};
+#include "fileviewer.h"
+#include "keydel.h"
+#include "messageserver.h"
+
+
+enum{
+    CAMERCTLMODE =  0,
+    LIGHTCTLMODE,
+    ROBOTCTLMODE,
+};
+
+enum{
+    MENUSTATEHIDE =  0,
+    MENUSTATESHOW,
+    MENUSTATESEC,
+    MENUSTATSETING,
+    MENUSTATPLAYING,
+    MENUSTATLOOKPHOTO,
+};
+
+
+namespace Ui {
+class Sec_Terminal;
+}
+
+class Sec_Terminal : public QWidget
+{
+    Q_OBJECT
+    
+public:
+    explicit Sec_Terminal(QWidget *parent = 0);
+    ~Sec_Terminal();
+    void slotActionSave     (void);
+    void IUpdateMode        (void);
+    int  MenuOneShow        (void);
+    void SecFileListDestory (void);
+    void MenuKeyUP          (void);
+    void MenuKeyDown        (void);
+    void MenuKeyReturn      (void);
+    void MenuKeyEnter       (void);
+    void GetPhotoOrVideo    (int Type);
+    int  IsEmptyFile        (char *file_name);
+    void ButtonKey_1_Func   (void);
+    void ButtonKey_2_Func   (void);
+    void ButtonKey_3_Func   (void);
+    void ButtonKey_4_Func   (void);
+    void ButtonKey_5_Func   (void);
+    void ButtonKey_11_Func  (int Type);
+
+    int TerminalPowerMen;//电量标志位
+    int LongPressFlag;//长恩标志位
+    int FIFOCmd;
+    int ICURRENTMODE;
+private slots:
+    void  timerDone(void);
+    void  PhyButtonDeal(int Num,int Type);
+    void  OsdUpdateFunc(int);
+private:
+    QLabel *RectGround;
+    QLabel *LittleRectGround;
+    QLabel *RectTitle;
+    QLabel *SpeedWatch;
+    QLabel *SpeedPoint;
+    QLabel *Direction;
+    QLabel *DirPoint;
+
+
+    QLabel *ShortKey_0;
+    QLabel *ShortKey_1;
+    QLabel *ShortKey_2;
+    QLabel *ShortKey_3;
+    QLabel *ShortKey_4;
+    QLabel *ShortKey_5;
+
+    QLabel *TerminalPower;
+    MessageServer msgServer;
+    int          PauseFlag;
+    /**************************参数显示**********************************************/
+    QLabel  *CarStateArgc;
+    QLabel  *FuYangSt;
+    QLabel  *HeGunSt;
+    QLabel  *HangXiangSt;
+    QLabel  *BaiBiSt;
+    QLabel  *WeDuSt;
+    QLabel  *SuDuSt;
+    QLabel  *SuDuDangWeiSt;
+    QLabel  *CameraAngle;
+    QLabel  *FangDaLab;
+    QLabel  *GuangMinLab;
+    QString BlowUpssStr;//放大字符串
+    int BlowUpssFlag;//放大标志位
+    int GuanMiZhiWei;
+    /*****************************文字*******************************************/
+    QLabel  *ModeStatePic;
+    /****************************航向*******************************************/
+    int HXpPointRoter;
+    QGraphicsScene* HXpScene;
+    QGraphicsView * HXpView;
+    /****************************码表*******************************************/
+    QGraphicsScene* MBpScene;
+    QGraphicsView * MBpView;
+    QLabel  *SpeedDescription ;
+    float MemMBpView;
+    /****************************灯光*******************************************/
+    QLabel *LightIconQ;
+    QLabel *LightIconH;
+    QLabel *LightIconS;
+    QLabel *LightIconY;
+
+    QListWidget *MenuList;          //菜单
+    int MenuListFlag;
+    QLabel *MenuDes;
+
+    int MenuStateFlag;
+    FileViewer *fileview;
+    char cmdbuf[128];
+
+    QListWidget *SecMenuList;          //Sec菜单
+    int          SeclistFlag;
+    int          VideoOrPicFlag;
+    int          FangDaOrGuangMi;
+
+    QLabel *ShowPhoto;
+    /*---------------音频---------------*/
+    int fd_gpio;
+    unsigned char gpiobuf[5];
+    int SetGPIO(int value); //write GPIO22
+    void SET_AUDIO_BYPASS();
+    void SET_AUDIO_368OUT();
+  //  unsigned char DetectTFCard(void);
+
+    /*---------------定时器--------------*/
+    QTimer *timer;
+    KeyDel *iButtonMsg;
+
+    int CararFlag;              //挡位Flag
+    Ui::Sec_Terminal *ui;
+};
+
+#endif // SEC_TERMINAL_H
---
+++ 二版本4轮软件/Sec_Terminal_pth5_升级/ui_sec_terminal.h
@@ -0,0 +1,50 @@
+/********************************************************************************
+** Form generated from reading UI file 'sec_terminal.ui'
+**
+** Created: Tue Jun 26 18:47:59 2018
+**      by: Qt User Interface Compiler version 4.7.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_SEC_TERMINAL_H
+#define UI_SEC_TERMINAL_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QHeaderView>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Sec_Terminal
+{
+public:
+
+    void setupUi(QWidget *Sec_Terminal)
+    {
+        if (Sec_Terminal->objectName().isEmpty())
+            Sec_Terminal->setObjectName(QString::fromUtf8("Sec_Terminal"));
+        Sec_Terminal->resize(400, 300);
+
+        retranslateUi(Sec_Terminal);
+
+        QMetaObject::connectSlotsByName(Sec_Terminal);
+    } // setupUi
+
+    void retranslateUi(QWidget *Sec_Terminal)
+    {
+        Sec_Terminal->setWindowTitle(QApplication::translate("Sec_Terminal", "Sec_Terminal", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Sec_Terminal: public Ui_Sec_Terminal {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_SEC_TERMINAL_H
